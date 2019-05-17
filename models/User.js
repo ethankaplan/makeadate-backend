9427506a8529c05621116987fc4f09244ca9f19f
Ethan <N/A>
2019-05-16 21:45:33 -0700

    expand user

--- models/User.js
+++ models/User.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose')
 
 const UserSchema = new mongoose.Schema({
   username: String,
-  password: String
+  password: String,
+  dates:[{location:String,activity:Object,dinner:Object}]
 })
 
 module.exports = mongoose.model('User', UserSchema)
